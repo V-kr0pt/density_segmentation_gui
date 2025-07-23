513646c8ef07ad54f0dc8f13ec4e21fbf2f0b672
Vitor França <vitor.sousa.franca@gmail.com>
2025-07-22 21:32:27 -0300

    add: saving mask in output with nii and png format and also a json file with the selected mask points and the scale used in the program to correct this points to the original image shape

--- src/app.py
+++ src/app.py
@@ -7,15 +7,23 @@ from utils import ImageLoader, MaskOperations
 def file_selector(folder_path=os.path.join(os.getcwd(), 'media')):
         filenames = os.listdir(folder_path)
         selected_filename = st.selectbox('Select a file', filenames)
-        return os.path.join(folder_path, selected_filename)
-        
+        return selected_filename, os.path.join(folder_path, selected_filename)
+
 def main():
     st.set_page_config(layout="wide")
     st.title("# Density Segmentation GUI")
 
+    # Clear all if requested
+    if st.button("Clean Section"):
+        st.session_state.clear()
+        st.success("Selection cleared.")
+
     # Select file and load image
-    file_path = file_selector()
-    image = ImageLoader.load_image(file_path)
+    selected_filename, file_path = file_selector()
+    image, affine = ImageLoader.load_image(file_path)
+
+    if "affine" not in st.session_state:
+        st.session_state["affine"] = affine
 
     # Define the maximum width and height for the image display
     max_width, max_height = 1200, 800
@@ -23,6 +31,10 @@ def main():
 
     # Calculate the scale to fit the image within the max dimensions
     scale = min(max_width / orig_width, max_height / orig_height, 1)
+
+    if "scale" not in st.session_state:
+        st.session_state["scale"] = scale
+    
     pil_width = int(orig_width * scale)
     pil_height = int(orig_height * scale)
 
@@ -32,16 +44,6 @@ def main():
     if "points" not in st.session_state:
         st.session_state.points = []
     
-        # Create canvas for drawing allowing to clear the section
-    if "clear_canvas" not in st.session_state:
-        st.session_state.clear_canvas = False
-
-    # Clear the canvas if requested
-    if st.button("Clean Section"):
-        st.session_state.points = []
-        st.session_state.clear_canvas = True
-        st.success("Selection cleared.")
-
     st.write("Draw a polygon on the image to segment:")
     
     # Render the canvas
@@ -57,10 +59,6 @@ def main():
         key="canvas"
     )
 
-    # Reset the clear flag after rendering the canvas
-    if st.session_state.clear_canvas:
-        st.session_state.clear_canvas = False
-
     if canvas_result.json_data is not None:
         objects = canvas_result.json_data["objects"]
         if len(objects) > 0:
@@ -69,9 +67,15 @@ def main():
             st.session_state.points = points
 
     if st.button("Create mask on the selected area"):
+        st.session_state["create_mask"] = True
+
+    if st.session_state.get("create_mask", False):
         if len(st.session_state.points) >= 3:
             # Create a mask from the drawn polygon
             result, mask = MaskOperations.create_mask(image, st.session_state.points, reduction_scale=scale)
+            st.session_state["mask"] = mask
+            st.session_state["result"] = result
+            st.session_state["output_path"] = os.path.join(os.getcwd(), 'output', selected_filename.split('.')[0])
 
             # Display the original image and the segmented area
             col1, col2 = st.columns(2)
@@ -80,15 +84,32 @@ def main():
             with col2:
                 st.image(result, caption="Segmented Area", use_container_width=True)
 
-            # Save the mask
-            if st.button("Save Mask"):
-                MaskOperations.save_mask(mask, file_path=os.path.join(os.getcwd(), 'output', 'mask.png'))
-                st.success("Mask saved successfully.")
-
+            st.session_state["create_mask"] = False
+            
         else:
             st.warning("Select at least 3 points.")
 
+    # Save the mask if requested
+    if st.button("Save Mask"):
+        st.session_state["save_mask"] = True
 
+    if st.session_state.get("save_mask", False):
+        st.session_state["save_mask"] = False
+        if "mask" in st.session_state:
+            st.write(f"Saving mask to: {st.session_state.output_path}")
+
+            # We have to create the local variables to avoid issues with Streamlit's session state
+            MaskOperations.save_mask(st.session_state.mask, affine=st.session_state.affine, 
+                                        file_path=st.session_state.output_path,
+                                        points=st.session_state.points, 
+                                        scale=st.session_state.scale)
+            st.success("Mask saved successfully.")
+            st.session_state.points = []
+            st.session_state.mask = None
+            st.session_state.result = None            
+        else:
+            st.error("No mask to save. Please create a mask first.")
+        
 
 if __name__ == "__main__":
     main()
